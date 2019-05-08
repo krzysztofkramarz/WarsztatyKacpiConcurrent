0b8c10c4637d7748085e59369243e329ce4e7622
Kacper Staszek <kacperstaszek1995@gmail.com>
2019-05-08 06:52:40 +0200

    poprawiony warsztat

--- src/main/java/cyclicbarrier/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Warsztaty z CyclicBarrier
- * Zapoznając się z przykładami i wykonując zadanie, posiądziesz podstawową wiedzę na temat działania i wykorzystania
- * tej klasy.
- *
- * @see java.util.concurrent.CyclicBarrier
- */
-package cyclicbarrier;
